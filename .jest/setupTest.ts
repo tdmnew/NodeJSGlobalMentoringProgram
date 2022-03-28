b6fdeadebc85715c812d1f5080180a192339ab1d
Tim New <Tim_New@epam.com>
2022-03-28 12:20:55 +0100

    removed comment

--- .jest/setupTest.ts
+++ .jest/setupTest.ts
@@ -1,12 +1,4 @@
 import 'regenerator-runtime/runtime';
-// import SequelizeMock from 'sequelize-mock';
-// const MockDB = new SequelizeMock();
-//
-// const mockGroup = MockDB.define('groups', {
-//     id: '1',
-//     name: 'Admin',
-//     permisions: ['READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD FILES']
-// });
 
 jest.mock('../src/config/index.ts', () => ({
     ENV_VARIABLES: {
